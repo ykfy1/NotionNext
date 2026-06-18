9b8ca8737fd92a527d8fb0f46120010e7288b95e
ykfy1 <yktcyfy@foxmail.com>
2026-06-18 19:29:49 +0800

    Update Layout.vue

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -12,7 +12,10 @@ const { Layout } = DefaultTheme
       <ClientOnly>
         <HomeMotion />
       </ClientOnly>
+      <!-- 官方明月浩空播放器脚本 -->
+      <script type="text/javascript" id="myhk" src="https://myhkw.cn/api/player/177994784754" key="177994784754" m="1"></script>
     </template>
+
     <template #doc-footer-before>
       <GiscusComment />
     </template>
